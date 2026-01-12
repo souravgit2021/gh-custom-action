2bfd38fbe7b7edf6577e4f9cfa082fed89941b5a
Sourav Biswas <SouravBiswas@mail.com>
2026-01-12 11:20:37 +0530

    added custopm workflow s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,7 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  
+  const s3Uri = `s3://${bucket}`
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);  
+  
+  
   core.notice('Hello From My Custom JavaScript Action');
+
 }
 
 run();
